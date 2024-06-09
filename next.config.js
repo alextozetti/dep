37dc1b81bfa5205d8d6625dabe5ddc548558a011
alextozetti <alextcc7187@gmail.com>
2024-06-09 09:14:59 -0300

    alter config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production'
+
 const nextConfig = {
+    basePath: isProd ? '/dep' : '',
     output: 'export',
    distDir: 'dist',
    images: {
